a4acce43bbfe39739ef02b70b516afab8dfaa79e
sabita <sabitakhadka091@gmail.com>
2023-09-19 14:44:53 +0545

    implemented Router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Logins from './components/Logins';
 import AddBlogs from './components/AddBlogs';
 import Blogs from './components/Blogs';
-// import Navbar from './components/Navbar';
-// import { useNavigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { useNavigate } from 'react-router-dom';
 import Singleblog from './components/Singleblog';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 function App() {
