e67f2b6abea7139faf92c5ea15ebcd0f2bbf7448
sabita <sabitakhadka091@gmail.com>
2023-09-22 11:36:38 +0545

    added forms

--- src/components/AddBlogs.jsx
+++ src/components/AddBlogs.jsx
@@ -1,43 +1,91 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { useState } from "react";
+import Navbar from "./Navbar";
 function AddBlogs() {
+  const [formData, setFormData] = useState({
+    title: "",
+    shortDescription: "",
+    fullDescription: "",
+    img: "",
+  });
+  const handleChange = (e) => {
+    console.log('handleChange is called');
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData.title);
+  };
   return (
     <>
-    <Navbar />
-    <div className="container" style={{
-      width:'700px',
-      marginTop:'40px',
-    
-    }}>
+      <Navbar />
+      <div
+        className="container"
+        style={{
+          width: "700px",
+          marginTop: "40px",
+        }}
+      >
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Title
+            </label>
+            <input 
+            value={formData.title}
+            name="title"
+            onChange={handleChange}
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
 
-    <form>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Title</label>
-    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-  </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Short Discription
+            </label>
+            <input
+            value={formData.shortDescription}
+            name="shortDescription"
+            onChange={handleChange}
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
 
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">
+              Full Discription
+            </label>
+            <textarea
+            value={formData.fullDescription}
+            name="fullDescription"
+            onChange={handleChange}
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
 
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Short Discription</label>
-    <input type="text" className="form-control" id="exampleInputPassword1"/>
-  </div>
-
-  <div className="mb-3">
-  <label for="exampleFormControlTextarea1" className="form-label">Full Discription</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-
-  <div className="mb-3 ">
-  <label for="exampleInputPassword1" className="form-label">img Url </label>
-  <input type="text" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  <button type="button" className="btn btn-primary">Add Blog</button>
-</form>
-  
-    </div>
-    
-      </>
-  )
+          <div className="mb-3 ">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              img Url{" "}
+            </label>
+            <input
+            value={formData.img}
+            name="img"
+            onChange={handleChange}
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="button" className="btn btn-primary">
+            Add Blog
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default AddBlogs
+export default AddBlogs;
